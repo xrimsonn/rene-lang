041a1007c1c9b661a9565b12bfa90b2f1d15857a
Antonio Rosales <jose.rosales.mald@gmail.com>
2024-10-17 19:36:45 -0600

    read file (not working)

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char** argv) {
-    printf("hello rene\n");
+    if (argv[1] == NULL) {
+        printf("Usage: rene <input_file>\n");
+    }
+
+    char * buffer = NULL;
+    size_t len;
+    FILE * fp = fopen(argv[1], "r");
+    const ssize_t bytes_read = getdelim(&buffer, &len, '\0', fp);
+    if (bytes_read != -1) {
+        printf("%s", buffer);
+    }
 }
